25eba057e5d50e7325cb3004f2594530d388b9d5
Jlit1001 <jordanlittell@yahoo.com>
2014-07-09 21:42:41 -0700

    adropdown menu, up format icon list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+$(window).resize(function(){
+	if ($(window).width()>=845){
+		$("#mobile-menu").hide();
+	}
+})
 
+
+$("#mobile-menu-img").click(function(event){
+	event.stopPropagation();
+	$("#mobile-menu").slideDown("slow");
+});
+$('body').click(function(event){
+	event.stopPropagation();
+	$("#mobile-menu").slideUp("slow");
+});
